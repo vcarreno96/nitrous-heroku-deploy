787068b2746abf34fa69d227664d5873e4e3a177
Victor Carreno <heatfan24@gmail.com>
2014-07-26 22:47:35 +0000

    change 2

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var http = require('http'),
+herokuvar http = require('http'),
     port = Number(process.env.PORT || 3000);
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello Noders DevOp');
+  res.end('Hello Noders DevOp 2');
 }).listen(port);
 
 console.log('Server running on port ' + port);
